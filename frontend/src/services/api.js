ce7d7f8be8e8916740da05d7d34f0a310f260d73
Luis Eduardo Teófilo <luiseduardocbft@gmail.com>
2026-06-08 14:56:45 -0300

    feat: integracao com o back-end

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/tarefas",
+  baseURL: "https://projeto-final-arquitetura-de-sistemas.onrender.com/",
 });
